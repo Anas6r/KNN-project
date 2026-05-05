e5d12208ef83be880ef284c452c0349f6f68d44d
Habib Shahriar Rahman Anas <155803109+Anas6r@users.noreply.github.com>
2026-05-05 15:04:54 +0600

    Import MNIST dataset and prepare train/test splits
    Added code to import MNIST dataset and split into train/test sets.

--- mnist.py
+++ mnist.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import fetch_openml
 
+mnist = fetch_openml('mnist_784', as_frame=False)
+
+x, y = mnist.data, mnist.target
+
+# test and train data:
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
