30ddac28d13a6f79c7d5769b76aaf018f74f6f68
Habib Shahriar Rahman Anas <155803109+Anas6r@users.noreply.github.com>
2026-08-08 23:39:04 +0600

    Remove unused imports from mnist.py
    Removed unused imports of pandas and numpy.

--- mnist.py
+++ mnist.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
