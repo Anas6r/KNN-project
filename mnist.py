6ae6f0863d828e2ad848931e41d064b290b9007d
Habib Shahriar Rahman Anas <155803109+Anas6r@users.noreply.github.com>
2026-08-08 23:28:30 +0600

    Fix variable naming for training and testing data

--- mnist.py
+++ mnist.py
@@ -11,7 +11,7 @@ model = KNeighborsClassifier()
 x, y = mnist.data, mnist.target
 
 # test and train data:
-X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+x_train, x_test, y_train, y_test = x[:60000], x[60000:], y[:60000], y[60000:]
 param_grid = [{'weights': ["uniform", "distance"], 'n_neighbors': [3, 4, 5, 6, 7, 2, 9, 8]}]
 model.score(x_test, y_test)
 knn = GridSearchCV(model, param_grid,cv=8)
